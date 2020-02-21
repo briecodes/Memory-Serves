3003645db000baef91dfe4a02e34f4a946cfa801
briecodes <hello@brie.work>
2020-02-21 11:31:53 -0500

    Added endgame logic.

--- src/Containers/Gameboard/Gameboard.js
+++ src/Containers/Gameboard/Gameboard.js
@@ -5,12 +5,15 @@ import Interstitial from '../../Components/Interstitial/Interstitial';
 
 export default function Gameboard(props) {
   const [difficulty, setDifficulty] = useState('');
+  const [gameEnd, setGameEnd] = useState(false);
 
   return (
     <>
       <button onClick={() => props.handlePageChange('home')}>go back</button>
       gameboard | {difficulty}
       {difficulty ? null : <Interstitial page={'difficulty'} setDifficulty={setDifficulty} handlePageChange={props.handlePageChange} />}
+
+      {gameEnd ? <Interstitial page={'endgame'} handlePageChange={props.handlePageChange} /> : null}
     </>
   );
 };
