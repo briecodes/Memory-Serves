fafbcd798882671dc33fb4d6096a179858f0ad41
briecodes <hello@brie.work>
2020-02-21 11:53:07 -0500

    Adding card logic.

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
-export default function Card() {
+export default function Card(props) {
+  const [active, setActive] = useState(false);
+
+  function handleClick() {
+    props.handleCompare('item');
+    setActive(!active);
+  };
+
   return (
-    <>
-    </>
+    <div className={`card set ${active ? 'active' : null}`} onClick={handleClick}>
+      Card
+    </div>
   );
 };
--- src/Containers/CardSpread/CardSpread.js
+++ src/Containers/CardSpread/CardSpread.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './CardSpread.css';
 
-export default function CardSpread() {
+import Card from '../../Components/Card/Card';
+
+export default function CardSpread(props) {
+  const set = useRef([]);
+  const cardCount = 30;
+  
+  function dealCards() {
+    const cardArray = [];
+    for (let i = 0; i <= cardCount; i++) {
+      cardArray.push(<Card key={i} handleCompare={compareSet} />);
+    };
+
+    return cardArray;
+  };
+
+  function compareSet(card) {
+    set.current.push(card);
+
+    if (set.current.length === 2) {
+      set.current[0] === set.current[1] ? console.log('score!') : console.log('NO SCORE');
+      set.current = [];
+    };
+  };
+
   return (<>
     CardSpread
+    {dealCards()}
   </>);
 };
