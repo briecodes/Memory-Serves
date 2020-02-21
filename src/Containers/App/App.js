1d95c3f96944603f665e0d04985f764d4c573847
briecodes <hello@brie.work>
2020-02-21 10:57:12 -0500

    Created gameboard. Begun app structure.

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import Interstitial from '../../Components/Interstitial/Interstitial';
+
 export default function App() {
+  const [page, setPage] = useState('');
+
+  function changePage(page) {
+    setPage(page);
+  };
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      {page != 'game' ? <Interstitial page={page} handlePageChange={changePage} /> : null}
+    </div>
   );
 };
---
+++ src/Containers/Gameboard/Gameboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Gameboard.css';
+
+export default function Gameboard() {
+  return (
+    <>
+    </>
+  );
+};
