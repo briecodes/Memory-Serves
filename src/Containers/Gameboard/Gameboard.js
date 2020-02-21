7851c8ccb00ee0b1f8cc32170dbfe4b64b867167
briecodes <hello@brie.work>
2020-02-21 11:35:56 -0500

    Added component to gameboard.

--- src/Containers/Gameboard/Gameboard.js
+++ src/Containers/Gameboard/Gameboard.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Gameboard.css';
 
 import Interstitial from '../../Components/Interstitial/Interstitial';
+import CardSpread from '../CardSpread/CardSpread';
 
 export default function Gameboard(props) {
   const [difficulty, setDifficulty] = useState('');
@@ -11,7 +12,7 @@ export default function Gameboard(props) {
     <>
       <button onClick={() => props.handlePageChange('home')}>go back</button>
       gameboard | {difficulty}
-      {difficulty ? null : <Interstitial page={'difficulty'} setDifficulty={setDifficulty} handlePageChange={props.handlePageChange} />}
+      {difficulty ? <CardSpread setGameEnd={setGameEnd} /> : <Interstitial page={'difficulty'} setDifficulty={setDifficulty} handlePageChange={props.handlePageChange} />}
 
       {gameEnd ? <Interstitial page={'endgame'} handlePageChange={props.handlePageChange} /> : null}
     </>
