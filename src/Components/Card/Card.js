7dd1fdacde6b7068eed276ca2d5da2ffae8435b9
briecodes <hello@brie.work>
2020-02-24 16:48:07 -0500

    Refactored card component for functioning card selection.

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Card.css';
 
 export default function Card(props) {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    return () => setActive(false);
+  }, [props.set]);
+
+  function handleClick() {
+    setActive(!active);
+    props.handleCompare(props.id);
+  };
 
   return (
-    <div className={`card set ${props.set.includes(props.id) ? 'active' : ''} ${props.wonSet.includes(props.id) ? 'won' : ''}`} onClick={() => props.handleCompare(props.id)}>
+    // <div className={`card set ${props.set.includes(props.id) ? 'active' : ''} ${props.wonSet.includes(props.id) ? 'won' : ''}`} onClick={() => props.handleCompare(props.id)}>
+    //   Card {props.id}
+    // </div>
+
+    <div className={`card set ${props.set.includes(props.id) || active ? 'active' : ''} ${props.wonSet.includes(props.id) ? 'won' : ''}`} onClick={handleClick}>
       Card {props.id}
     </div>
   );
--- src/Containers/CardSpread/CardSpread.js
+++ src/Containers/CardSpread/CardSpread.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './CardSpread.css';
 
 import Card from '../../Components/Card/Card';
 
 export default function CardSpread(props) {
-  // const [wonSet, setWonSet] = useState([]);
-  // const [set, setSet] = useState([]);
   const [cardSets, setCardSets] = useState({set: [], wonSet: []});
+  const compareTimeout = useRef();
   
   function dealCards() {
     const cardArray = [];
@@ -20,16 +19,19 @@ export default function CardSpread(props) {
   };
 
   function compareSet(card) {
-    // setSet([...set, card]);
-    setCardSets({set: [...cardSets.set, card], wonSet: [...cardSets.wonSet]});
+    if (cardSets.set.length === 0) {
+      setCardSets({set: [card], wonSet: [...cardSets.wonSet]});
+    } else if (cardSets.set.length === 1) {
 
-    if (cardSets.set.length === 2) {
-      if (cardSets.set[0].split('-')[0] === cardSets.set[1].split('-')[0]) {
-        // setWonSet([...wonSet, ...set]);
-        setCardSets({set: [], wonSet: [...cardSets.wonSet, ...cardSets.set]});
-      } else {
-        
-      };
+      compareTimeout.current = window.setTimeout(() => {
+        // setCardSets({sets: [...cardSets.set, card], wonSet: [...cardSets.wonSet]});
+
+        if (cardSets.set[0].split('-')[0] === card.split('-')[0]) {
+          setCardSets({set: [], wonSet: [...cardSets.wonSet, ...cardSets.set, card]});
+        }else {
+          setCardSets({set: [], wonSet: [...cardSets.wonSet]});
+        };
+      }, 500);
     };
   };
 
